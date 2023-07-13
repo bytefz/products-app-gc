1017e4d92113949b223084420982083b6486ba80
Franzua Plasencia <franzua.15pc@gmail.com>
2023-07-12 22:18:13 -0500

    Add a new route

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -5,13 +5,18 @@ import 'package:products_app/features/products/products.dart';
 import 'package:products_app/features/auth/presentation/presentation.dart';
 
 final goRouter = GoRouter(
-  initialLocation: '/login',
+  initialLocation: Routes.login,
   routes: [
     ///* Auth Routes
     GoRoute(
       path: Routes.login,
       builder: (context, state) => const LoginScreen(),
     ),
+    GoRoute(
+      path: Routes.updateInfoUser,
+      builder: (context, state) => const UpdateInfoUserScreen(),
+    ),
+    //TODO: Add Confirmation Screen
 
     ///* Product Routes
     GoRoute(
--- lib/config/router/routes.dart
+++ lib/config/router/routes.dart
@@ -1,4 +1,8 @@
 class Routes {
+  //? Auth Routes
   static const String login = '/login';
+  static const String updateInfoUser = '/update-info-user';
+
+  //? Product Routes
   static const String product = '/';
 }
