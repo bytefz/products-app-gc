b4d2dd77565fd457132a45bd34403af1124c2e66
Franzua Plasencia <franzua.15pc@gmail.com>
2023-07-15 16:19:05 -0500

    End Confirmation View

--- lib/features/auth/presentation/screens/confirmation_view.dart
+++ lib/features/auth/presentation/screens/confirmation_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ConfirmationView extends StatelessWidget {
@@ -12,14 +14,115 @@ class ConfirmationView extends StatelessWidget {
 }
 
 class _ConfirmationView extends StatelessWidget {
-  const _ConfirmationView({super.key});
+  const _ConfirmationView();
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Center(
-        child: Text('ConfirmationView'),
+        child: Column(
+          children: [
+            Spacer(),
+            _ImageConfirmation(),
+            _ContentTextWidget(),
+            Spacer(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _ContentTextWidget extends StatefulWidget {
+  const _ContentTextWidget();
+
+  @override
+  State<_ContentTextWidget> createState() => _ContentTextWidgetState();
+}
+
+class _ContentTextWidgetState extends State<_ContentTextWidget> {
+  String points = '.';
+
+  StreamController<String> streamController =
+      StreamController<String>.broadcast();
+
+  _changeLengthPoints() async {
+    Future.delayed(const Duration(seconds: 1), () {
+      if (streamController.isClosed) return;
+      streamController.add(points);
+
+      if (points.length < 3) {
+        points += '.';
+        streamController.add(points);
+        _changeLengthPoints();
+        return;
+      }
+
+      points = '.';
+      streamController.add(points);
+      _changeLengthPoints();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _changeLengthPoints();
+  }
+
+  @override
+  void dispose() async {
+    streamController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    const name = "Antonio";
+
+    return StreamBuilder<String>(
+      initialData: '.',
+      stream: streamController.stream,
+      builder: (context, snapshot) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Text(
+                'Hola $name,',
+                textAlign: TextAlign.center,
+                style: textTheme.titleMedium,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Espera un momento, estamos confirmando tu cuenta${snapshot.data}',
+                textAlign: TextAlign.center,
+                style: textTheme.bodyLarge?.copyWith(
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ImageConfirmation extends StatelessWidget {
+  const _ImageConfirmation();
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Image.asset(
+      'assets/imgs/desk-lamp.png',
+      width: size.width * 0.7,
+      height: size.height * 0.4,
+      filterQuality: FilterQuality.high,
     );
   }
 }
