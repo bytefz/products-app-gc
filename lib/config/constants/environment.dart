b29d319b57059793e4c7fe91df13411453d7a37b
Franzua Plasencia <franzua.15pc@gmail.com>
2023-07-15 21:59:33 -0500

    Add New Variables

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -5,5 +5,12 @@ class Environment {
     await dotenv.load(fileName: ".env");
   }
 
+  // WhatsApp URLs
+  static String androidWhatsappUrl = dotenv.env['ANDROID_WHATSAPP_URL'] ??
+      'No ANDROID_WHATSAPP_URL env variable';
+  static String iosWhatsappUrl =
+      dotenv.env['IOS_WHATSAPP_URL'] ?? 'No IOS_WHATSAPP_URL env variable';
+
+  // API URLs
   static String apiUrl = dotenv.env['API_URL'] ?? 'No API_URL env variable';
 }
