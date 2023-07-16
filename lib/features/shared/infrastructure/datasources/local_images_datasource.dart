23e44504acaabb0d02bd72a1c4b817dd61ae4a74
Franzua Plasencia <franzua.15pc@gmail.com>
2023-07-15 22:01:27 -0500

    Add New Images in DataSource

--- lib/features/shared/infrastructure/datasources/local_images_datasource.dart
+++ lib/features/shared/infrastructure/datasources/local_images_datasource.dart
@@ -1,6 +1,7 @@
 class LocalImagesDataSource {
   // IMAGES
   static get loginImagePath => 'assets/imgs/login.png';
+  static get deskLampImagePath => 'assets/imgs/desk-lamp.png';
 
   // LOGOS
   static get googleLogoPath => 'assets/logos/google.png';
