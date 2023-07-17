590e489a32f6d5c180f73e9db10cf52a248a0eb8
Franzua Plasencia <franzua.15pc@gmail.com>
2023-07-17 01:24:48 -0500

    Add a AppBar in InventaryScreen

--- lib/features/inventary/presentation/screens/inventary_screen.dart
+++ lib/features/inventary/presentation/screens/inventary_screen.dart
@@ -5,11 +5,44 @@ class InventaryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Product Screen'),
+      appBar: AppBar(
+        title: Text(
+          'Inventary Screen',
+          style: textTheme.titleMedium?.copyWith(
+            color: colors.onPrimary,
+          ),
         ),
+        foregroundColor: colors.onPrimary,
+        backgroundColor: colors.primary,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.filter_list_rounded),
+            onPressed: () {},
+          ),
+        ],
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(10),
+          ),
+        ),
+      ),
+      body: const _InventaryView(),
+    );
+  }
+}
+
+class _InventaryView extends StatelessWidget {
+  const _InventaryView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Inventary Screen'),
       ),
     );
   }
